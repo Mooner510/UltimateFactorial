a345eef45222de76b78b39ae690dcdc9bd42bd3d
Mooner510 <lmck510@gmail.com>
2022-05-25 16:23:14 +0900

    I like me

--- src/Threads/Main.java
+++ src/Threads/Main.java
@@ -1,8 +1,5 @@
 package Threads;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 public class Main {
     public static String fileLocation = "factorial-{VALUE}.txt";
     public static final String value = "100m";
